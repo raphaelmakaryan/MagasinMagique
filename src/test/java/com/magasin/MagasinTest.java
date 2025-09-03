a5547fc1a44b5683aedf1dd87905c8938ac2798e
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-09-03 14:16:01 +0200

    Refactorisation propre avec une nouvelle méthode newUpdateQuality et sa version de comparaison

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -9,7 +9,7 @@ class MagasinTest {
     void foo() {
         Item[] items = new Item[]{new Item("Pouvoirs magiques", 0, 54), new Item("Comté", 10, 0), new Item("Pass VIP Concert", 1, 1), new Item("Kryptonite", 10, 80)};
         Magasin app = new Magasin(items);
-        //app.newUpdateQualityCommented();
+        //app.newUpdateQuality();
         Assertions.assertEquals("Pouvoirs magiques", app.items[0].name);
     }
 
@@ -27,7 +27,7 @@ class MagasinTest {
         //Item[] items = new Item[]{new Item("Pouvoirs magiques", 0, 25), new Item("Comté", 10, 0), new Item("Pass VIP Concert", 1, 1), new Item("Kryptonite", 10, 80)};
         Item[] items = new Item[]{new Item("Pouvoirs magiques", 0, 25)};
         Magasin app = new Magasin(items);
-        app.newUpdateQualityNotCommented();
+        app.newUpdateQuality();
         Assertions.assertEquals("Pouvoirs magiques", app.items[0].name);
     }
 
